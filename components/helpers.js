2a48935452e236ee8034e52233412914dabbd9be
Brock McElroy <28877984+brxck@users.noreply.github.com>
2019-05-08 13:41:53 -0700

    Add hours to timestamp

--- components/helpers.js
+++ components/helpers.js
@@ -1,5 +1,11 @@
 export const formatSeconds = seconds => {
-  const mm = new String(Math.floor(seconds / 60)).padStart(2, '0')
-  const ss = new String(Math.floor(seconds % 60)).padStart(2, '0')
-  return `${mm}:${ss}`
+  const hh = Math.floor(seconds / 3600).toString()
+  const mm = Math.floor((seconds % 3600) / 60)
+    .toString()
+    .padStart(2, '0')
+  const ss = Math.floor(seconds % 60)
+    .toString()
+    .padStart(2, '0')
+
+  return `${hh == '0' ? '' : `${hh}:`}${mm}:${ss}`
 }
