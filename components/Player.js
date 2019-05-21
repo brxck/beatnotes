97b9957073f6b768863f2357b7e423b72019bd91
Brock McElroy <28877984+brxck@users.noreply.github.com>
2019-05-20 20:11:32 -0700

    Add input for changing media url

--- components/Player.js
+++ components/Player.js
@@ -1,21 +1,36 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import ReactPlayer from 'react-player'
 import PropTypes from 'prop-types'
 
 const Player = ({ playerRef }) => {
+  const urlRef = useRef()
+  const [url, setUrl] = useState('https://www.youtube.com/watch?v=AgpWX18dby4')
+
   return (
     <div style={{ display: 'flex', justifyContent: 'center' }}>
-      <ReactPlayer
-        url="https://www.youtube.com/watch?v=AgpWX18dby4"
-        ref={playerRef}
-        controls
-      />
+      <ReactPlayer url={url} ref={playerRef} controls />
+      <div>
+        <input
+          type="text"
+          name="url"
+          id="url"
+          ref={urlRef}
+          defaultValue={url}
+        />
+        <button
+          onClick={() => {
+            setUrl(urlRef.current.value)
+          }}
+        >
+          go
+        </button>
+      </div>
     </div>
   )
 }
 
 Player.propTypes = {
-  playerRef: PropTypes.shape.isRequired({
+  playerRef: PropTypes.shape({
     current: PropTypes.instanceOf(Element),
   }),
 }
