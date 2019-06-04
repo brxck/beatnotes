c62b0a1300a4e8624426802f579d1c5e6d2b52eb
Brock McElroy <28877984+brxck@users.noreply.github.com>
2019-06-04 10:22:24 -0700

    Add help toggle

--- components/Player.js
+++ components/Player.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import ReactPlayer from 'react-player'
 import PropTypes from 'prop-types'
 import { css } from '@emotion/core'
@@ -8,25 +8,45 @@ import { GlobalContext } from './GlobalState'
 function Player({ playerRef }) {
   const { globalState } = useContext(GlobalContext)
   const { url } = globalState
+  const [helpVisible, setHelpVisible] = useState(true)
   return (
     <>
       <ReactPlayer url={url} ref={playerRef} controls width="100%" />
 
-      <div
-        css={css`
-          color: #555;
-          font-size: 0.8rem;
-        `}
-      >
-        <p>
-          Supports YouTube, Vimeo, Facebook, SoundCloud, Dailymotion, & Twitch.
-        </p>
-        <p>
-          Press <i>shift + enter</i> to add a newline to the same time range.
-        </p>
-        <p>
-          Type <i>%t</i> to insert a timestamp.
-        </p>
+      <div style={{ position: 'relative' }}>
+        <button
+          onClick={() => setHelpVisible(previous => !previous)}
+          css={css`
+            background-color: transparent;
+            border: none;
+            right: 0;
+            padding: 0.2rem;
+            position: absolute;
+            font-size: 0.8rem;
+          `}
+        >
+          <span role="img" aria-label="help">
+            {helpVisible ? '✖' : '❓'}
+          </span>
+        </button>
+        <div
+          css={css`
+            color: #555;
+            font-size: 0.8rem;
+            visibility: ${helpVisible ? 'visible' : 'hidden'};
+          `}
+        >
+          <p>
+            Supports YouTube, Vimeo, Facebook, SoundCloud, Dailymotion, &
+            Twitch.
+          </p>
+          <p>
+            Press <i>shift + enter</i> to add a newline to the same time range.
+          </p>
+          <p>
+            Type <i>%t</i> to insert a timestamp.
+          </p>
+        </div>
       </div>
     </>
   )
